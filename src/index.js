f79ff9ee665c73ef93bc5bea1080f97a7326e02b
Elijah Ally <elijahally18@gmail.com>
2021-07-29 20:00:32 -0400

    finished solutions and contact modals

--- src/index.js
+++ src/index.js
@@ -10,13 +10,15 @@ import './css/navbar.css';
 import './css/reset.css';
 import './css/main.css';
 import './css/modals.css';
-import './css/contact-form.css';
+import './css/contact-modal.css';
 import './css/welcome-modal.css';
+import './css/solutions-modal.css';
+import './css/about-modal.css';
 
 handleModals();
 
 if (!localStorage.getItem('pageNum')) {
-	localStorage.setItem('pageNum', '1')
+	localStorage.setItem('pageNum', '1');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
--- src/util/modals.js
+++ src/util/modals.js
@@ -3,59 +3,73 @@ const modalBackground = document.getElementsByClassName('modal-background')[0];
 const modalForeground = document.getElementsByClassName('modal-foreground')[0];
 
 // modal content
-const welcomeModal = document.getElementById('welcome-modal')
-const aboutModal = document.getElementById('about-modal')
-const solutionsModal = document.getElementById('solutions-modal')
-const contactModal = document.getElementById('contact-modal')
-const closeModal = document.getElementById('close-modal')
-const countdown = document.getElementById('countdown')
+const welcomeModal = document.getElementById('welcome-modal');
+const aboutModal = document.getElementById('about-modal');
+const solutionsModal = document.getElementById('solutions-modal');
+const contactModal = document.getElementById('contact-modal');
+const closeModal = document.getElementById('close-modal');
+const countdown = document.getElementById('countdown');
 
 // modal nav links
-const modalLinks = document.getElementsByClassName('modal-links');
-const aboutLink = modalLinks[0];
-const solutionsLink = modalLinks[1];
-const contactLink = modalLinks[2];
+const modalLinks = document.getElementsByClassName('nav-links');
+const welcomeLink = modalLinks[0];
+const aboutLink = modalLinks[1];
+const solutionsLink = modalLinks[2];
+const contactLink = modalLinks[3];
 
 export const handleModals = () => {
-    // display welcome modal on page load if its first page load
-    if (!parseInt(localStorage.getItem('pageNum'))) {
-        modalBackground.style.display = 'block'
-        modalForeground.style.display = 'flex'
-        welcomeModal.style.display = 'flex'
-    
-        countdown.style.display = 'flex'
-    }
+	let openedWelcomeModal = false;
+	// display welcome modal on page load if its first page load
+	if (!parseInt(localStorage.getItem('pageNum'))) {
+		openedWelcomeModal = true;
+		modalBackground.style.display = 'block';
+		modalForeground.style.display = 'flex';
+		welcomeModal.style.display = 'flex';
 
-    
-    // open modal logic
-    aboutLink.addEventListener('click', () => openModal(aboutModal))
-    solutionsLink.addEventListener('click', () => openModal(solutionsModal))
-    contactLink.addEventListener('click', () =>  openModal(contactModal))
-    
-    // closing modal logic
-    modalBackground.addEventListener('click', () => closeAllModals())
-    closeModal.addEventListener('click', () => closeAllModals())
-    //close welcome modal after 7 seconds
-    setTimeout(() => closeAllModals(), 7000);
-}
+		countdown.style.display = 'flex';
+	}
 
-const openModal = (modal) => {
-    modal.style.display = 'flex'
+	// open modal logic
+	welcomeLink.addEventListener('click', () =>
+		openModal(welcomeModal, countdown)
+	);
+	aboutLink.addEventListener('click', () => openModal(aboutModal));
+	solutionsLink.addEventListener('click', () => openModal(solutionsModal));
+	contactLink.addEventListener('click', () => openModal(contactModal));
 
-    modalBackground.style.display = 'block'
-    modalForeground.style.display = 'flex'
-    closeModal.style.display = 'flex'
-}
+	// closing modal logic
+	modalBackground.addEventListener('click', () => closeAllModals());
+	closeModal.addEventListener('click', () => closeAllModals());
+
+	//close welcome modal after 7 seconds
+	if (openedWelcomeModal) setTimeout(() => closeAllModals(), 7000);
+};
+
+const openModal = (modal, countdownTimer = null) => {
+	welcomeModal.style.display = 'none';
+
+	modal.style.display = 'flex';
+	modalBackground.style.display = 'block';
+	modalForeground.style.display = 'flex';
+
+	if (countdownTimer) {
+		countdown.style.display = 'flex';
+		return;
+	}
+
+	countdown.style.display = 'none';
+	closeModal.style.display = 'flex';
+};
 
 const closeAllModals = () => {
-    if (modalBackground.style.display === 'block') {
-        modalBackground.style.display = 'none'
-        modalForeground.style.display = 'none'
-    
-        welcomeModal.style.display = 'none'
-        aboutModal.style.display = 'none'
-        solutionsModal.style.display = 'none'
-        contactModal.style.display = 'none'
-        closeModal.style.display = 'none'
-    }
-}
+	if (modalBackground.style.display === 'block') {
+		modalBackground.style.display = 'none';
+		modalForeground.style.display = 'none';
+
+		welcomeModal.style.display = 'none';
+		aboutModal.style.display = 'none';
+		solutionsModal.style.display = 'none';
+		contactModal.style.display = 'none';
+		closeModal.style.display = 'none';
+	}
+};
