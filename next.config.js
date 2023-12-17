e3fd8a5872c1e456e05de342b3b2bd5feaba0e6e
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2023-12-17 16:12:51 -0500

    updated next config for ghpages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Use the prefix for GitHub Pages
+  basePath: '/SimplifySpaceJunk',
+  assetPrefix: '/SimplifySpaceJunk/',
+  output: 'standalone',
 }
 
 module.exports = nextConfig
